c6c1e6f804fd7f9637d50688f077015d8409a93d
msharm33 <42849159+msharm33@users.noreply.github.com>
2018-12-01 09:02:19 -0500

    Update vuln_test.php

--- vuln_test.php
+++ vuln_test.php
@@ -1,6 +1,6 @@
 <?php
 $searchStr = 'This is a test string.';
-$toReplace = $_GET['toreplace'];
+$toReplace = "/".$_GET['toreplace']."/";
 $replaceWith = $_GET['replacewith'];
 
 echo preg_replace($toReplace, $replaceWith, $searchStr);
