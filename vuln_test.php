8d963fe563770213eb89149e41cd16b43aa717ce
msharm33 <42849159+msharm33@users.noreply.github.com>
2018-12-01 09:50:38 -0500

    Update vuln_test.php

--- vuln_test.php
+++ vuln_test.php
@@ -2,5 +2,5 @@
 $searchStr = 'This is a test string.';
 $toReplace = "test";
 $replaceWith = "ls";
-echo preg_replace($toReplace, $replaceWith, $searchStr);
+echo preg_replace("/".$toReplace."/", $replaceWith, $searchStr);
 ?>
