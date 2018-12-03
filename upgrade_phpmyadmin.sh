be288222fbeb559886acf9c4ad34fd54083b65af
msharm33 <42849159+msharm33@users.noreply.github.com>
2018-12-03 07:36:04 -0500

    Create upgrade_phpmyadmin.sh

---
+++ upgrade_phpmyadmin.sh
@@ -0,0 +1,13 @@
+sudo rm -rf /usr/share/phpmyadmin
+
+sudo wget https://files.phpmyadmin.net/phpMyAdmin/4.4.15.10/phpMyAdmin-4.4.15.10-english.zip --no-check-certificate
+
+unzip phpMyAdmin-4.4.15.10-english.zip
+
+sudo mv phpMyAdmin-4.4.15.10-english /usr/share/phpmyadmin
+
+sudo mkdir /usr/share/phpmyadmin/tmp
+
+sudo chown www-data /usr/share/phpmyadmin/tmp
+
+sudo cp /usr/share/phpmyadmin/config.sample.inc.php  /usr/share/phpmyadmin/config.inc.php
