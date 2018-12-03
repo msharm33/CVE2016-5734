99c634caf8e46bc528bb10e98786324f07942890
Geet Sawhney <geet_sawhney14@yahoo.co.in>
2018-12-02 20:53:35 -0500

    adding warning that will be thrown

--- vuln_test_execute.php
+++ vuln_test_execute.php
@@ -1,9 +1,11 @@
 <?php
 $searchStr = 'This is a test string.';
+
 // Without Null Character
   //$toReplace = "test/e";
-// The upper won't work
-  
+// The upper code won't work
+// and "Warning: preg_replace(): Unknown modifier '/'" will be thrown
+
 $toReplace = "test/e\0";
 
 $replaceWith = "system('whoami')";
